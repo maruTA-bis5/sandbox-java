e6a750ed61cd8309285b4c9e71cb0d3e6ca2e7bc
Takayuki Maruyama <bis5.wsys@gmail.com>
2018-11-13 13:09:43 +0900

    [commons-configuration] YAMLのサンプルを追加

--- commons-configuration/src/main/java/net/bis5/sandbox/App.java
+++ commons-configuration/src/main/java/net/bis5/sandbox/App.java
@@ -2,6 +2,7 @@ package net.bis5.sandbox;
 
 import org.apache.commons.configuration2.*;
 import org.apache.commons.configuration2.ex.*;
+import org.apache.commons.configuration2.builder.*;
 import org.apache.commons.configuration2.builder.fluent.*;
 
 public class App {
@@ -15,5 +16,14 @@ public class App {
 		Configurations configurations = new Configurations();
 
 		return configurations.properties(propertyFile);
+	}
+
+	public Configuration loadYaml(String yamlFile) throws ConfigurationException {
+		BasicConfigurationBuilder<YAMLConfiguration> builder = new FileBasedConfigurationBuilder<>(YAMLConfiguration.class) //
+			.configure(new Parameters().hierarchical() //
+					.setFileName(yamlFile) //
+					);
+
+		return builder.getConfiguration();
 	}
 }
--- commons-configuration/src/test/java/net/bis5/sandbox/AppTest.java
+++ commons-configuration/src/test/java/net/bis5/sandbox/AppTest.java
@@ -11,7 +11,7 @@ public class AppTest {
 		String fileName = "config.ini";
 
 		Configuration conf = new App().loadIni(fileName);
-		conf.getKeys().forEachRemaining(System.err::println);
+		// conf.getKeys().forEachRemaining(System.err::println);
 
 		assertTrue(conf.containsKey(ConfigKey.BACKEND_URL.get()));
 		assertTrue(conf.containsKey(ConfigKey.LOGIN_USER.get()));
@@ -23,10 +23,23 @@ public class AppTest {
 		String fileName = "config.properties";
 
 		Configuration conf = new App().loadProperties(fileName);
+		// conf.getKeys().forEachRemaining(System.err::println);
+
+		assertTrue(conf.containsKey(ConfigKey.BACKEND_URL.get()));
+		assertTrue(conf.containsKey(ConfigKey.LOGIN_USER.get()));
+		assertTrue(conf.containsKey(ConfigKey.LOGIN_PASSWORD.get()));
+	}
+
+	@Test
+	public void testLoadYaml() throws ConfigurationException {
+		String fileName = "config.yaml";
+
+		Configuration conf = new App().loadYaml(fileName);
 		conf.getKeys().forEachRemaining(System.err::println);
 
 		assertTrue(conf.containsKey(ConfigKey.BACKEND_URL.get()));
 		assertTrue(conf.containsKey(ConfigKey.LOGIN_USER.get()));
 		assertTrue(conf.containsKey(ConfigKey.LOGIN_PASSWORD.get()));
 	}
+
 }
