724575b08da66e202292da647bd26471632a0f04
Maruyama Takayuki <maruyama@bbreak.co.jp>
2019-11-13 19:45:25 +0900

    make serializable and assign initial value to field

--- sonarqube/sonarjava-fp-test/src/main/java/net/bis5/s1948/S1948App2.java
+++ sonarqube/sonarjava-fp-test/src/main/java/net/bis5/s1948/S1948App2.java
@@ -1,14 +1,11 @@
 package net.bis5.s1948;
 
 import org.primefaces.model.*;
+import java.io.*;
 
-public class S1948App2 {
+public class S1948App2 implements Serializable {
 
-    private TreeNode rootNode;
-
-    public S1948App2() {
-        rootNode = new DefaultTreeNode("root node", null);
-    }
+    private TreeNode rootNode = new DefaultTreeNode("root node", null);
 
     public TreeNode getRootNode() {
         return rootNode;
