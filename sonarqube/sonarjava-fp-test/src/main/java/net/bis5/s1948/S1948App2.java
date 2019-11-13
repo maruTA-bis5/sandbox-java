04779e7a3f24a5abacd5ae137fb95efbdec1863d
Maruyama Takayuki <maruyama@bbreak.co.jp>
2019-11-13 19:56:09 +0900

    add final property

--- sonarqube/sonarjava-fp-test/src/main/java/net/bis5/s1948/S1948App2.java
+++ sonarqube/sonarjava-fp-test/src/main/java/net/bis5/s1948/S1948App2.java
@@ -7,7 +7,13 @@ public class S1948App2 implements Serializable {
 
     private TreeNode rootNode = new DefaultTreeNode("root node", null);
 
+    private final TreeNode finalRootNode = new DefaultTreeNode("root node", null);
+
     public TreeNode getRootNode() {
         return rootNode;
+    }
+
+    public TreeNode getFinalRootNode() {
+        return finalRootNode;
     }
 }
